4901a2e6b2cabba7cf1d5a24442aef9cd90625df
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2021-02-14 11:57:00 +0300

    Surafel: sidebar menu added to all pages & bug fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,29 @@
-// set date dynamically
-var datePicker = document.querySelector('.slide-banner input[type="date"]');
-let today = new Date().toISOString().substr(0, 10);
-datePicker.value = today;
-
-// owl carousel for news section
-
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: false,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  dots: false,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 2,
-    },
-    1000: {
-      items: 3,
-    },
-  },
-});
-
 // Nav bar scroll listner
 const preHeader = document.querySelector('.pre-header');
 const headerBar = document.querySelector('#landing-header');
+
+// Humberger menu
+const humToggle = document.querySelector('.humberger-menu');
+const sideBar = document.querySelector('.sidebar');
+const content = document.querySelector('.content-wrapper');
+const overlay = document.querySelector('#overlay');
+var overlayEffect = true;
+humToggle.addEventListener('click', (e) => {
+  sideBar.classList.toggle('slide');
+  if (overlayEffect) {
+    overlay.style.display = "block";
+    overlayEffect = false;
+  } else {
+    overlay.style.display = "none";
+    overlayEffect = true;
+  }
+});
+
+overlay.addEventListener('click', (e) => {
+  sideBar.classList.remove('slide');
+  overlay.style.display = "none";
+  overlayEffect = true;
+})
 
 window.addEventListener('scroll', (e) => {
   let navHeight = Number(headerBar.getBoundingClientRect().bottom);
@@ -38,31 +34,3 @@ window.addEventListener('scroll', (e) => {
   }
 });
 
-$(document).ready(function () {
-  $('#sidebarCollapse').on('click', function () {
-    $('#sidebar').toggleClass('active');
-  });
-});
-
-// Toggle account animation
-const accountToggleL = document.querySelector('.account-toggle.l span');
-const accountToggleR = document.querySelector('.account-toggle.r span');
-const formContainer = document.querySelector('.form-container');
-accountToggleL.addEventListener('click', (e) => {
-  e.preventDefault();
-  formContainer.parentElement.classList.add('form-anim-l');
-  formContainer.parentElement.classList.remove('form-anim-r');
-});
-accountToggleR.addEventListener('click', (e) => {
-  e.preventDefault();
-  formContainer.parentElement.classList.remove('form-anim-l');
-  formContainer.parentElement.classList.add('form-anim-r');
-});
-
-// Humberger menu
-const humToggle = document.querySelector('.humberger-menu');
-const sideBar = document.querySelector('.sidebar');
-humToggle.addEventListener('click', (e) => {
-  console.log(sideBar.classList);
-  sideBar.classList.toggle('slide');
-});

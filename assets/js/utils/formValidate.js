d610e98a3f89c76ef3e41f3c0b4c8efbe8f7730d
nabek <nabekabebe6@gmail.com>
2021-02-15 14:19:17 +0300

    Nabek: form validation utility functions and constants added.

---
+++ assets/js/utils/formValidate.js
@@ -0,0 +1,16 @@
+function validateForm(type, vlaue) {
+  switch (type) {
+    case INPUT_TYPE.name:
+      break;
+    case INPUT_TYPE.email:
+      break;
+    case INPUT_TYPE.phone:
+      break;
+    case INPUT_TYPE.zip:
+      break;
+    case INPUT_TYPE.credit_card:
+      break;
+    default:
+      break;
+  }
+}
